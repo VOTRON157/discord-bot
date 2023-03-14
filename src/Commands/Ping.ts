3fb428305f6d72adcb7f40b9e8a650ce5c97bd34
VOTRON157 <bonitoclash609@gmail.com>
2023-03-13 22:13:26 -0300

    Ping melhorado

--- src/Commands/Ping.ts
+++ src/Commands/Ping.ts
@@ -7,6 +7,6 @@ export default new class implements Command {
     .setName('ping')
     .setDescription('A ping command!')
     public run = (client: typeof Bot, interaction: UserContextMenuCommandInteraction) => {
-        interaction.reply(`🏓 Pong! meu ping é: ${client.ws.ping}ms`)
+        interaction.reply(`🏓 Pong!\n\n🚀 Ping do websocket: \`\`${client.ws.ping}ms\`\`\n⌚ Seu comando levou cerca de \`\`${Math.abs((new Date()).getMilliseconds() - interaction.createdAt.getMilliseconds())}ms\`\` para ser respondido`)
     }
 }

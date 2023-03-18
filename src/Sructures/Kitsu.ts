0927b660af0f04082d37b1719ce32372b30318b9
VOTRON157 <bonitoclash609@gmail.com>
2023-03-18 13:22:16 -0300

    Removido o tipo

--- src/Sructures/Kitsu.ts
+++ src/Sructures/Kitsu.ts
@@ -1,44 +1,13 @@
 import axios from 'axios'
 
-interface NecessaryAPIResponse { // ainda vai dar pra acessar as outra propriedades que retornam da API, so fiz isso pro vscode mostrar no auto completar
-    data: [{
-        id: string
-        type: string
-        attributes: {
-            titles: {
-                en: string
-                en_jp: string
-                ja_jp: string
-            }
-            episodeCount: string
-            synopsis: string
-            posterImage: {
-                tiny: string
-                large: string
-                small: string
-                medium: string
-                original: string
-            }
-        }
-        relationships: {
-            genres: {
-                links: {
-                    related: string
-                }
-            }
-        }
-        genres: Array<string>
-    }]
-}
-
 export default class KistsuAPI {
     private baseURL = 'https://kitsu.io/api/edge/'
 
-    public async findAnimes(animeName: string): Promise<NecessaryAPIResponse> {
+    public async findAnimes(animeName: string): Promise<any> {
         const response = await axios.get(this.baseURL + 'anime?filter[text]=' + encodeURIComponent(animeName))
         return response.data
     }
-    public async findAnime(animeName: string): Promise<NecessaryAPIResponse['data'][0]> {
+    public async findAnime(animeName: string): Promise<any> {
         const animes = await this.findAnimes(animeName)
         const anime = animes.data[0]
         const response = await axios.get(anime.relationships.genres.links.related)
